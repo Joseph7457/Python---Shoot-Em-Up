c0b51091e60270e9b1fc154af6e7df13a694dbbb
senrim17 <91601744+senrim17@users.noreply.github.com>
2021-10-05 22:12:59 +0200

    Dictionnaire entite et suppression des tirs hors écran
    Création d'un dictionnaire "entité" et de ses fonctions, définition du joueur et des tirs comme des entites, suppression des tirs hors écran

--- shtemup.py
+++ shtemup.py
@@ -16,7 +16,7 @@ ship_position = [FENETRE_LARGEUR//2, FENETRE_HAUTEUR-100]
 
 #Tirs
 TIR_VITESSE   = -1; TIR_TAILLE    = 3
-tir_positions = []
+tirs = []
 
 #Cooldowns
 cooldown_tir = 200 # Millisecondes entre chaque tir
@@ -31,6 +31,28 @@ fenetre_taille = (FENETRE_LARGEUR, FENETRE_HAUTEUR)
 fenetre = pygame.display.set_mode(fenetre_taille)
 fenetre.fill(NOIR)
 
+#--- Définition entite
+
+def creer_entite():
+    return {
+        'position': [0, 0],
+        'vitesse': [0, 0]
+    }
+
+def position(entite):
+    return entite['position']
+
+def vitesse(entite):
+    return entite['vitesse']
+
+def modifier_vitesse(entite, vecteur):
+    entite['vitesse'][X] = vecteur[X]
+    entite['vitesse'][Y] = vecteur[Y]
+
+def modifier_position(entite, coordonnees):
+    entite['position'][X] = coordonnees[X]
+    entite['position'][Y] = coordonnees[Y]
+
 #Definition fonctions
 
 #Tirs joueurs
@@ -39,37 +61,61 @@ def tirer():
     global cooldown_tir, temps_dernier_tir
     if((tir == True) and (pygame.time.get_ticks() - temps_dernier_tir >= cooldown_tir)):
 
-       temps_dernier_tir = pygame.time.get_ticks()
-       tp = [ship_position[0], ship_position[1]]
-       tir_positions.append(tp)
+        temps_dernier_tir = pygame.time.get_ticks()
+
+        position_joueur = position(joueur)
+
+        nouveau_tir = creer_entite()
+        modifier_position(nouveau_tir, position_joueur)
+        modifier_vitesse(nouveau_tir, (0, TIR_VITESSE))
+        tirs.append(nouveau_tir)
+
 def afficher_tir():
-    for i in range(len(tir_positions)):
-        pygame.draw.circle(fenetre, BLANC, tir_positions[i], TIR_TAILLE)
+    for i in range(len(tirs)):
+        pygame.draw.circle(fenetre, BLANC, position(tirs[i]), TIR_TAILLE)
 
 def effacer_tir():
-    for i in range(len(tir_positions)):
-        pygame.draw.circle(fenetre, NOIR, tir_positions[i], TIR_TAILLE)
+    for i in range(len(tirs)):
+        pygame.draw.circle(fenetre, NOIR, position(tirs[i]), TIR_TAILLE)
 
 def deplacer_tir():
-    for i in range(len(tir_positions)):
-        tir_positions[i][1] += deplacement_1d(TIR_VITESSE)
+    for i in range(len(tirs)):
+        deplacer(tirs[i])
+
+def detruire_tir():
+    index_tirs_a_supprimer = []
+    for i in range(len(tirs)):
+        if position(tirs[i])[Y] < 0:
+            index_tirs_a_supprimer.append(i)
+    
+    for i in range(len(index_tirs_a_supprimer)):
+        del tirs[index_tirs_a_supprimer[i]]
 
 #deplacement       
-
-def deplacer_joueur(vitesse):
-       ship_position[0] += deplacement_1d( (-int(gauche)+int(droite)) * vitesse)
-
-# --- Retourne une liste contenant la distance parcourue pour chaque dimension
-def deplacement_2d(vitesse): #must be list
-       return [deplacement_1d(vitesse[X]), deplacement_1d(vitesse[Y])]
 
 # --- Retourne la distance parcourue sur une dimension pendant un tour de boucle
 def deplacement_1d(vitesse):
     return vitesse * temps.get_time()
 
+# --- Retourne une liste contenant la distance parcourue pour chaque dimension
+def deplacement_2d(vitesse): #must be list
+       return [deplacement_1d(vitesse[X]), deplacement_1d(vitesse[Y])]
+
+# --- Fonctions de deplacement utilisant les entites
+
+def translation(entite, mouvement):
+    entite['position'][X] += mouvement[X]
+    entite['position'][Y] += mouvement[Y]
+
+def deplacer(entite):
+    translation(entite, deplacement_2d(vitesse(entite)) )
+
 # ----- Fin définitions fonctions
 
 temps = pygame.time.Clock()
+
+joueur = creer_entite()
+modifier_position(joueur, (ship_position[0], ship_position[1]))
 
 while not fini:
 
@@ -93,25 +139,27 @@ while not fini:
             if evenement.key == pygame.K_SPACE :
                tir = False
 
+    modifier_vitesse(joueur, (SHIP_VITESSE * -int(gauche)+int(droite), 0) )
 
     #effacer
-    pygame.draw.circle(fenetre, NOIR, ship_position, SHIP_TAILLE)
+    pygame.draw.circle(fenetre, NOIR, position(joueur), SHIP_TAILLE)
     effacer_tir()
     
     #actions
         
-    deplacer_joueur(SHIP_VITESSE)
+    deplacer(joueur)
     deplacer_tir()
+    detruire_tir()
     tirer()
     
     #dessiner
-    pygame.draw.circle(fenetre, BLANC, ship_position, SHIP_TAILLE)
+    pygame.draw.circle(fenetre, BLANC, position(joueur), SHIP_TAILLE)
     afficher_tir()
     pygame.display.flip()
-    print(temps.get_fps())
-    print(temps.get_time())
 
     temps.tick(60)
+
+    print(len(tirs))
 
 
 
